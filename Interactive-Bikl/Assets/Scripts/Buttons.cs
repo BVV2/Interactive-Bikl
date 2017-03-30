43015a0f3f97c5d01b57861ce0c7fac36008224e
@BVV2 <bvv2ldskt@mail.ru>
2017-03-31 01:37:53 +0300

    Revert "Fungus platform upload"
    This reverts commit e1f7d0071e65add0c7e91a0e1ccfa4c7ee8f7df5.

---
+++ Interactive-Bikl/Assets/Scripts/Buttons.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+    
+    public Scene SC;
+ 
+    public void Game1()
+    {
+        SceneManager.LoadScene("Game1");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void HomeS()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void Selector()
+    {
+        SceneManager.LoadScene("Selector");
+    }
+    /*
+    public void NextLevel()
+    {
+        SC = SceneManager.GetActiveScene();        
+        string name = SC.name;
+        Debug.Log("current scene " + name);
+        string nextName = "Game " + (int.Parse(name[name.Length + 1].ToString()) + 1).ToString();
+        Debug.Log("loading scene " + nextName);
+        SceneManager.LoadScene(nextName);
+    }
+    */
+
+    public void Restart()
+    {
+        SC = SceneManager.GetActiveScene();
+        string name = SC.name;
+        Debug.Log("loading scene " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    
+
+}
